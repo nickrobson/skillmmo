373bf4e4b306dca3b36d632499ea3c22b68b83b4
Nick Robson <robson.nicholas1@gmail.com>
2021-11-21 21:09:05 +1100

    Delete remaining old skill unlocks classes

--- src/main/java/dev/nickrobson/minecraft/skillmmo/data/SkillMmoDataType.java
+++ src/main/java/dev/nickrobson/minecraft/skillmmo/data/SkillMmoDataType.java
@@ -2,7 +2,6 @@ package dev.nickrobson.minecraft.skillmmo.data;
 
 public final class SkillMmoDataType<T extends DataValidatable> {
     public static final SkillMmoDataType<SkillData> SKILLS = new SkillMmoDataType<>("skills", SkillData.class);
-    public static final SkillMmoDataType<SkillLevelUnlocksData> SKILL_UNLOCKS = new SkillMmoDataType<>("skillunlocks", SkillLevelUnlocksData.class);
 
     private final String resourceCategory;
     private final Class<T> resourceClass;
