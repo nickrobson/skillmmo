149a8596f46cb8f9c0605399c21979ef0ba27d29
Nick Robson <robson.nicholas1@gmail.com>
2022-01-31 23:29:31 +1100

    🛠  Replace Redirect with Inject in first-login mixin

--- src/main/java/dev/nickrobson/minecraft/skillmmo/mixin/MixinPlayerManager.java
+++ src/main/java/dev/nickrobson/minecraft/skillmmo/mixin/MixinPlayerManager.java
@@ -10,25 +10,25 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(PlayerManager.class)
 public abstract class MixinPlayerManager {
-    @Redirect(
-            method = "onPlayerConnect",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;loadPlayerData(Lnet/minecraft/server/network/ServerPlayerEntity;)Lnet/minecraft/nbt/NbtCompound;")
+    @Inject(
+            method = "loadPlayerData",
+            at = @At("RETURN"),
+            locals = LocalCapture.CAPTURE_FAILEXCEPTION
     )
-    public NbtCompound skillMmo$loadPlayerData(PlayerManager playerManager, ServerPlayerEntity player) {
-        NbtCompound nbt = playerManager.loadPlayerData(player);
-        if (nbt == null) {
+    public void skillMmo$loadPlayerData(ServerPlayerEntity player, CallbackInfoReturnable<NbtCompound> cir, NbtCompound nbtCompound) {
+        if (nbtCompound == null) {
             // This is the first time the player has joined the server,
             // so initialise them with empty data
             ((SkillMmoPlayerDataHolder) player).setSkillMmoPlayerData(
                     new SkillMmoPlayerDataHolder.SkillMmoPlayerData()
             );
         }
-        return nbt;
     }
 
     @Inject(
