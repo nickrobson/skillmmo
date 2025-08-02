774a033c9f4d1061c834dad3b3ccb36a7c12413b
Nick Robson <robson.nicholas1@gmail.com>
2025-08-03 08:42:21 +1000

    Update to Minecraft 1.20.4

--- src/main/java/dev/nickrobson/minecraft/skillmmo/mixin/client/MixinConnectScreen.java
+++ src/main/java/dev/nickrobson/minecraft/skillmmo/mixin/client/MixinConnectScreen.java
@@ -5,7 +5,7 @@ import dev.nickrobson.minecraft.skillmmo.skill.SkillManager;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
 import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.network.ServerInfo;
 import org.spongepowered.asm.mixin.Mixin;
