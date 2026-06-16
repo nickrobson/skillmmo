c5b0a9cd676be9c4027be85670f686c74eaab97b
Nick Robson <robson.nicholas1@gmail.com>
2026-06-16 14:43:30 +1000

    ⚡️ SkillMMO v0.1.13

--- src/main/java/dev/nickrobson/minecraft/skillmmo/SkillMmoMod.java
+++ src/main/java/dev/nickrobson/minecraft/skillmmo/SkillMmoMod.java
@@ -24,7 +24,7 @@ public class SkillMmoMod implements ModInitializer {
 
     public static final String MOD_ID = "skillmmo";
     public static final String MOD_NAME = "SkillMMO";
-    public static final String MOD_VERSION = "0.1.12";
+    public static final String MOD_VERSION = "0.1.13";
     public static final String MOD_VERSION_STRING = MOD_NAME + " v" + MOD_VERSION;
 
     @Override
